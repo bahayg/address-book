6388a29d24bc15ab86efd17398176c6c6d77f569
Student <example@example.com>
2019-08-27 11:19:57 -0700

    Update attachContactListeners() method to delete Contact when button is clicked, update HTML to use bootstrap styles and clear form fields after submission.

--- js/scripts.js
+++ js/scripts.js
@@ -72,6 +72,11 @@ function attachContactListeners() {
   $("ul#contacts").on("click", "li", function() {
     showContact(this.id);
   });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
 };
 $(document).ready(function() {
   attachContactListeners();
@@ -80,6 +85,9 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
     var newContact = new Contact (inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
